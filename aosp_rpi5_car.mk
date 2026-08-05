6f7f7a5eb7f3730d0cfdcfa8ae15c0fd1e699b59
Dennis Field <dennisf@radiosound.com>
2026-08-05 10:48:13 -0400

    Grant Caramel Voice microphone access by default

--- aosp_rpi5_car.mk
+++ aosp_rpi5_car.mk
@@ -139,7 +139,8 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.activities_on_secondary_displays.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.activities_on_secondary_displays.xml \
-    frameworks/native/data/etc/car_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/car_core_hardware.xml
+    frameworks/native/data/etc/car_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/car_core_hardware.xml \
+    $(DEVICE_PATH)/permissions/default-permissions-rpi5.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions-rpi5.xml
 
 # Vehicle
 PRODUCT_PACKAGES += \
